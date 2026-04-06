7471173cddbb491ee39bc664f7e55ba9a503b27d
ana_mantella <146769187+anaclaramantellaa@users.noreply.github.com>
2026-04-06 20:07:21 -0300

    Add files via upload

---
+++ Aula6.java
@@ -0,0 +1,26 @@
+public class Aula6 {
+
+    /* primeiro exercicio de fixação
+    static void AlterarVetor(int[] vetor){
+        vetor[0] = 15;
+    }*/
+
+    /* 
+    static void FixacaoDois(int num){
+        num = 10;
+        System.out.println(num);
+    }
+    */
+    public static void main(String[] args) {
+
+        /*int[] num = {20, 30, 40};
+        AlterarVetor(num);
+        System.out.println(num[0]);*/
+    
+        /* 
+        int x = 33;
+        FixacaoDois(x);
+        System.out.println(x);*/
+
+    }
+}
